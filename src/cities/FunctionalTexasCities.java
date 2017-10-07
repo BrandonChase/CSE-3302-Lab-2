77e4728ea8135a682d8e8bcb637a0f14fc600e66
BrandonChase <coding@brandonchase.me>
2017-10-07 01:42:23 -0500

    Everything Working

--- src/cities/FunctionalTexasCities.java
+++ src/cities/FunctionalTexasCities.java
@@ -4,10 +4,16 @@
 package cities;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class FunctionalTexasCities
 {
@@ -16,13 +22,77 @@ public class FunctionalTexasCities
 	
 	public static void main(String[] args) throws FileNotFoundException, IOException
 	{
-		ArrayList<texasCitiesClass> texasCities = new ArrayList<texasCitiesClass>();
-		populateListFromFile(texasCities);
+		ArrayList<texasCitiesClass> texasCities = new ArrayList<>();
 
+		populateListsFromFile(texasCities);
+		
+		ArrayList<String> counties = texasCities.stream()
+				.map(city -> city.getCounty())
+				.distinct()
+				.sorted()
+				.collect(Collectors.toCollection(ArrayList::new));
+		
+		BufferedWriter bWriter = new BufferedWriter(new FileWriter(OUTPUT_FILE_NAME));
+		bWriter.write("County name\tNo. Cities\tTotal Pop\tAve Pop\tLargest City\tPopulation");
+		bWriter.newLine();
+		
+		counties.stream()
+				.forEach(county -> {
+					try {
+						bWriter.write(county);
+						
+						bWriter.write("\t");
+						
+						bWriter.write(String.valueOf(texasCities.stream()
+								.filter(city -> city.getCounty().equals(county))
+								.count()));
+						
+						bWriter.write("\t");
+						
+						bWriter.write(NumberFormat.getNumberInstance(Locale.US).format(
+								texasCities.stream()
+								.filter(city -> city.getCounty().equals(county))
+								.mapToInt(city -> city.getPopulation())
+								.sum()));
+						
+						bWriter.write("\t");
+						
+						bWriter.write(NumberFormat.getNumberInstance(Locale.US).format(
+								(int) texasCities.stream()
+								.filter(city -> city.getCounty().equals(county))
+								.mapToInt(city -> city.getPopulation())
+								.average()
+								.getAsDouble()));
+						
+						bWriter.write("\t");
+						
+						bWriter.write(texasCities.stream()
+								.filter(city -> city.getCounty().equals(county))
+								.max(Comparator.comparing(city -> city.getPopulation()))
+								.get()
+								.getName());
+						
+						bWriter.write("\t");
+						
+						bWriter.write(NumberFormat.getNumberInstance(Locale.US).format(
+								texasCities.stream()
+								.filter(city -> city.getCounty().equals(county))
+								.mapToInt(city -> city.getPopulation())
+								.max()
+								.getAsInt()));
+						
+						bWriter.newLine();
+					}
+					catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					});
+		bWriter.close();
 		
 	}
 	
-	static void populateListFromFile(ArrayList<texasCitiesClass> texasCities) throws FileNotFoundException, IOException
+	static void populateListsFromFile(ArrayList<texasCitiesClass> texasCities) throws FileNotFoundException, IOException
 	{
 		String[] values;
 		String line = "";
